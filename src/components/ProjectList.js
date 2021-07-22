0ca8b35621a09ec01f875e5cf4e713fee2b31916
TenzingS <thesherpatenzing@gmail.com>
2021-07-22 10:04:51 -0400

    Completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,22 @@
-import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const projectId = projects.map((project) => {
+    return (
+    <div key= {project.id}>
+      <ul>
+        <h3>{project.name}</h3>
+        <p>{project.about}</p>
+        <ProjectItem technologies = {project.technologies} />
+      </ul>
+    </div>)
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projectId}
+      </div>
     </div>
   );
 }
